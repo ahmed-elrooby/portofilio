73e44b88b299b60ef4f8362e7facbc0a549575c9
rooby <frontahmed099@gmail.com>
2024-12-26 12:02:01 +0200

    ss

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,86 @@
+"use client";
+import React from "react";
+import contact from "../../Images/contact.json";
+
+import { FaFilePdf, FaRegUser } from "react-icons/fa";
+import { IoIosHome, IoMdMail } from "react-icons/io";
+import { HiMiniPresentationChartLine } from "react-icons/hi2";
+import Link from "next/link";
+import successAnimation from "../../Images/loading.json"
+import { motion } from 'framer-motion';
+import Form from "./Form";
+// import Form from "./Form";
+import Lottie from "react-lottie-player";
+
+const Contact = () => {
+
+
+
+
+  return (
+    <>
+    <div className="relative  w-[95%] mx-auto mt-[80px] md:mt-[120px] md:mb-[50px] mb-[40px]">
+    <div className="text-center p-4   mb-6">
+  <h1 className="text-2xl font-bold text-[--main-color] mb-2">
+    Get in Touch with Us!
+  </h1>
+  <p className="text-gray-600 leading-[1.7] dark:text-gray-300">
+    We're here to help. If you have any questions or suggestions, feel free to reach out. 
+    Our team is ready to respond to your inquiries as quickly as possible.
+  </p>
+</div>
+      <div className="fixed z-[26622] border-2 border-[--main-color]  rounded-full px-1  md:px-3 h-[250px] flex flex-col gap-[5px] md:gap-[10px]  items-center justify-center left-1  md:left-4 md:top-[40%] top-[30%] text-[--main-color]">
+      <Link className="transition-all hover:text-gray-500 dark:hover:text-white " href="/"><IoIosHome size={36}/></Link>
+      <Link className="transition-all hover:text-gray-500 dark:hover:text-white  " href="/About"> <FaRegUser size={36}/></Link>
+      <Link className="transition-all hover:text-gray-500 dark:hover:text-white  " href="/Contact"> <IoMdMail size={36}/></Link>
+      <Link className="transition-all hover:text-gray-500 dark:hover:text-white  " href="/Projects"> <HiMiniPresentationChartLine size={36}/></Link>
+      <Link className="transition-all hover:text-gray-500 dark:hover:text-white  " href="/Resume"> <FaFilePdf size={36}/></Link>
+        
+       
+      
+      
+           </div>
+    <div>
+        <div className="flex flex-wrap items-center justify-center gap-[20px] md:gap-[30px] ">
+        <motion.div
+                initial={{ scale: 0.5 }}
+                transition={{
+                  duration: 1.3,
+                  type: "tween",
+                }}
+                whileInView={{
+                  scale: 1,
+                }}
+          
+          >
+            <Lottie
+              animationData={contact}
+              loop={true}
+              play
+              className=" w-full  md:w-[450px] md:h-[350px]  "
+            />
+          </motion.div>
+          <motion.div
+                initial={{ scale: 0.5 }}
+                transition={{
+                  duration: 1.3,
+                  type: "tween",
+                }}
+                whileInView={{
+                  scale: 1,
+                }}
+
+          
+          >
+<Form/>
+          </motion.div>
+          
+        </div>
+      </div>
+    </div>
+
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/components/Contact/Form.jsx
@@ -0,0 +1,89 @@
+"use client"
+import React, { useState } from 'react'
+import emailjs from '@emailjs/browser';
+import { FaSpinner } from 'react-icons/fa'
+// import toast from 'react-hot-toast';
+
+const Form = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    const [isLoading, setIsLoading] = useState(false); 
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsLoading(true); 
+        const serviceId = "service_ir59559";
+        const templateId = "template_96f4trf";
+        const publicKey = "tlHTnRvLGWyOzYULD";
+
+        const templateParams = {
+            from_name: name,
+            from_email: email,
+            to_name: "Ahmed ElRooby",
+            message: message
+        }
+
+        emailjs.send(serviceId, templateId, templateParams, publicKey)
+            .then((response) => {
+                if(response.status === 200){
+                    toast.success("Your message has been sent successfully 🥳!")
+                }
+                setName("");
+                setEmail("");
+                setMessage("");
+                setIsLoading(false); 
+            })
+            .catch((err) => {
+                console.log(err);
+                setIsLoading(false); 
+            });
+    }
+
+    return <>
+        <form
+            onSubmit={handleSubmit}
+            action=""
+            className="flex flex-col gap-3 p-3 rounded-lg md:gap-5 md:p-5"
+        >
+            <input
+                onChange={(e) => { setName(e.target.value) }}
+                required
+                className="outline-[--main-color] bg-transparent text-[--main-color] font-bold md:py-2 py-1 px-3 md:px-5 rounded-lg border-2 border-[--main-color]"
+                type="text"
+                value={name}
+                name="userName"
+                placeholder="Enter your name"
+                id="userName"
+            />
+            <input
+                onChange={(e) => { setEmail(e.target.value) }}
+                required
+                className="outline-[--main-color] bg-transparent text-[--main-color] font-bold md:py-2 py-1 px-3 md:px-5 rounded-lg border-2 border-[--main-color]"
+                type="email"
+                name="email"
+                placeholder="Enter your Email"
+                id="email"
+                value={email}
+            />
+            <textarea
+                onChange={(e) => { setMessage(e.target.value) }}
+                value={message}
+                className="outline-[--main-color] bg-transparent w-[300px] md:w-[400px] h-[250px] text-[--main-color] font-bold md:py-3 py-2 md:px-5 px-3 rounded-lg border-2 border-[--main-color]"
+                name=""
+                id=""
+                placeholder="Write your message"
+            ></textarea>
+
+            <button
+                type="submit"
+                className={`py-1 md:px-2 px-1 flex justify-center items-center text-center text-[18px] md:text-[22px] border-1 border-[--main-color] text-white cursor-pointer transition-all hover:bg-[#0B3D88] rounded-full bg-[--main-color] font-bold ${isLoading ? "opacity-70 cursor-not-allowed" : ""}`}
+                disabled={isLoading} 
+            >
+                {isLoading ? <FaSpinner className='animate-spin'/> : "Send"} 
+            </button>
+        </form>
+    </>
+}
+
+export default Form;
